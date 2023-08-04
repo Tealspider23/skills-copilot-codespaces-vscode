e096b7c933916d758e29500f6422bfd8d97d601a
Amit Kumar <123721031+Tealspider23@users.noreply.github.com>
2023-08-04 05:13:32 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsmember(){
+    var skills = document.getElementById("skills");
+    var member = document.getElementById("member");
+    var skillsmember = document.getElementById("skillsmember");
+    var member = document.getElementById("member");
+    var skillsmember = document.getElementById("skillsmember");
+}

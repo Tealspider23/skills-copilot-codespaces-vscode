978285805e4c05cf13afa7c9a5202ab6898e80f7
Amit Kumar <123721031+Tealspider23@users.noreply.github.com>
2023-08-04 10:51:01 +0530

    Delete member.js

--- member.js
+++
@@ -1,7 +0,0 @@
-function skillsMember(){
-    var skills = document.getElementById("skills");
-    var member = document.getElementById("member");
-    var skillsMember = document.getElementById("skillsMember");
-    var member = document.getElementById("member");
-    var skillsMember = document.getElementById("skillsMember");
-}
